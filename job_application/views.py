31bf16abe36b0cf57b534ffa2e34ea6a089e50c3
misha <misha07.sharma@gmail.com>
2025-04-02 19:31:05 +0530

    7th commit: Send an Email with Django

--- job_application/views.py
+++ job_application/views.py
@@ -2,20 +2,32 @@ from django.shortcuts import render
 from .forms import ApplicationForms
 from .models import Form
 from django.contrib import messages
+from django.core.mail import EmailMessage
 
 # Create your views here.
 
 def index(request):
     if request.method == "POST":
+
         form = ApplicationForms(request.POST)
         if form.is_valid():
+
+            # Getting user input with help of Application form class specified in forms.py
             first_name = form.cleaned_data["first_name"]
             last_name = form.cleaned_data["last_name"]
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
 
+            # Insert form data into the database
             Form.objects.create(first_name=first_name,last_name=last_name,email=email,date=date,occupation=occupation)
+
+            # Create email content and sending mail
+            message_body = f"Your details submitted successfully.. Thank you!! {first_name}"
+            email_message = EmailMessage(subject="Form Submission Confirmation!!",body=message_body,to=[email])
+            email_message.send()
+
+            # Flash message post sending mail and submit button clicked
             messages.success(request,"Your Form Submitted successfully!!")
 
     return render(request,"index.html")
--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import EMAIL_BACKEND, EMAIL_HOST, EMAIL_PORT, \
+    EMAIL_USE_TLS, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "sharmabruno310@gmail.com"
+EMAIL_HOST_PASSWORD = "gime jybb kcan rdgq"
